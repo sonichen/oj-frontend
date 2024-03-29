a8ead6735d69cf152353a1071bc58e1329f8960f
sonichen <1606673007@qq.com>
2024-03-29 14:52:05 +0800

    update access config

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -16,7 +16,7 @@
             <div class="title">OJ</div>
           </div>
         </a-menu-item>
-        <a-menu-item v-for="item in routes" :key="item.path">
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
           {{ item.name }}
         </a-menu-item>
       </a-menu>
@@ -36,10 +36,27 @@ export default {
 <script setup lang="ts">
 import { routes } from "../router/routes";
 import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 const router = useRouter();
+// 展示在菜单的路由数组
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    // 根据权限过滤菜单
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
 // 默认主页
 const selectedKeys = ref(["/"]);
 
@@ -53,8 +70,8 @@ console.log();
 
 setTimeout(() => {
   store.dispatch("user/getLoginUser", {
-    userName: "Soni",
-    role: "admin",
+    userName: "soni",
+    userRole: ACCESS_ENUM.ADMIN,
   });
 }, 3000);
 const doMenuClick = (key: string) => {

8c5fbb9304f546636972b3f65beda7096dab8e9f
sonichen <1606673007@qq.com>
2024-04-04 12:29:07 +0800

    add online coding

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-// config Component library arco.design
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
+import router from "./router";
+import store from "./store";
+import "@/plugins/axios";
+import "@/access";
 import "bytemd/dist/index.css";
-// Use
+
 createApp(App).use(ArcoVue).use(store).use(router).mount("#app");
